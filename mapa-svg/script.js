8b2acad68ea13ab1048d87e5c80b035c6a34da89
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-06-27 20:06:33 -0300

    Add files via upload

--- mapa-svg/script.js
+++ mapa-svg/script.js
@@ -15,8 +15,19 @@ async function loadMapData(){
     let mapaConteudo = document.querySelector('#map-box');
     mapaConteudo.innerHTML = mapaMalha;
 
+	// 1. seleciona todos os paths do svg (= id do mapaUrl)
+	// 2. para cada path eventListener de hover
+	// 3. dispara a funcao mostraHover(), criada fora
+
     let elemMunicipios = document.querySelectorAll('#mapa-conteudo svg path');
 }
 loadMapData();
+
+function mostraHover(){
+	// 1. get target = id do path
+	let uf = event.target.id
+	// 2. para uf de mapaDados > id (que ta no json) = uf
+	// 3. agora pega a uf e coloca no hover
+}
 
 // 2h01min
